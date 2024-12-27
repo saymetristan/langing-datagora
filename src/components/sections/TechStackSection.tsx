70a60bf9070a5e9d40c73e0ac7228ff9f3fb9ea2
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2024-12-27 03:52:42 -0600

    teckstack v2

--- src/components/sections/TechStackSection.tsx
+++ src/components/sections/TechStackSection.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useRef } from 'react'
 import { Space_Grotesk } from 'next/font/google'
 import OrbitingCircles from "@/components/ui/orbiting-circles"
-import { FaPython, FaReact, FaAws, FaDocker } from 'react-icons/fa'
-import { SiTensorflow, SiOpenai } from 'react-icons/si'
+import { FaPython, FaReact, FaAws, FaNode } from 'react-icons/fa'
+import { SiOpenai, SiHuggingface, SiSupabase, SiRedis, SiPostgresql, SiNextdotjs, SiElevenlabs, SiMeta } from 'react-icons/si'
+import { BiLogoPostgresql } from 'react-icons/bi'
 
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'],
@@ -20,57 +20,42 @@ export default function TechStackSection() {
 
       <div className="relative w-full max-w-4xl aspect-square">
         {/* Círculo interior */}
-        <OrbitingCircles
-          className="size-[40px] text-blue-400"
-          duration={20}
-          radius={100}
-        >
+        <OrbitingCircles className="size-[40px] text-blue-400" duration={20} radius={100}>
           <SiOpenai className="w-8 h-8" />
         </OrbitingCircles>
-        <OrbitingCircles
-          className="size-[40px] text-purple-400"
-          duration={20}
-          delay={5}
-          radius={100}
-        >
-          <SiTensorflow className="w-8 h-8" />
+        <OrbitingCircles className="size-[40px] text-purple-400" duration={20} delay={3} radius={100}>
+          <SiHuggingface className="w-8 h-8" />
+        </OrbitingCircles>
+        
+        {/* Círculo medio */}
+        <OrbitingCircles className="size-[40px] text-purple-400" duration={22} radius={150}>
+          <SiElevenlabs className="w-8 h-8" />
+        </OrbitingCircles>
+        <OrbitingCircles className="size-[40px] text-cyan-400" duration={22} delay={4} radius={150}>
+          <SiSupabase className="w-8 h-8" />
+        </OrbitingCircles>
+        <OrbitingCircles className="size-[40px] text-blue-400" duration={22} delay={8} radius={150}>
+          <SiRedis className="w-8 h-8" />
+        </OrbitingCircles>
+        <OrbitingCircles className="size-[40px] text-purple-400" duration={22} delay={12} radius={150}>
+          <BiLogoPostgresql className="w-8 h-8" />
         </OrbitingCircles>
 
         {/* Círculo exterior */}
-        <OrbitingCircles
-          className="size-[40px] text-cyan-400"
-          radius={200}
-          duration={25}
-          reverse
-        >
+        <OrbitingCircles className="size-[40px] text-cyan-400" radius={200} duration={25} reverse>
           <FaPython className="w-8 h-8" />
         </OrbitingCircles>
-        <OrbitingCircles
-          className="size-[40px] text-blue-400"
-          radius={200}
-          duration={25}
-          delay={6}
-          reverse
-        >
+        <OrbitingCircles className="size-[40px] text-blue-400" radius={200} duration={25} delay={5} reverse>
+          <FaNode className="w-8 h-8" />
+        </OrbitingCircles>
+        <OrbitingCircles className="size-[40px] text-purple-400" radius={200} duration={25} delay={10} reverse>
+          <SiNextdotjs className="w-8 h-8" />
+        </OrbitingCircles>
+        <OrbitingCircles className="size-[40px] text-cyan-400" radius={200} duration={25} delay={15} reverse>
           <FaReact className="w-8 h-8" />
         </OrbitingCircles>
-        <OrbitingCircles
-          className="size-[40px] text-purple-400"
-          radius={200}
-          duration={25}
-          delay={12}
-          reverse
-        >
+        <OrbitingCircles className="size-[40px] text-blue-400" radius={200} duration={25} delay={20} reverse>
           <FaAws className="w-8 h-8" />
-        </OrbitingCircles>
-        <OrbitingCircles
-          className="size-[40px] text-cyan-400"
-          radius={200}
-          duration={25}
-          delay={18}
-          reverse
-        >
-          <FaDocker className="w-8 h-8" />
         </OrbitingCircles>
       </div>
     </div>
